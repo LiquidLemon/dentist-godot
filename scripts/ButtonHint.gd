f8d1653e3212cc5b8337c21a839cd6b6fcc2a6e8
LiquidLemon <liquidlemon@protonmail.com>
2020-02-01 19:32:18 +0100

    Add basic transitions

--- scripts/ButtonHint.gd
+++ scripts/ButtonHint.gd
@@ -3,9 +3,11 @@ extends AnimatedSprite
 export (String) var action
 
 func _process(delta):
+	var holding = self.get_node("Holding")
 	if Input.is_action_just_pressed(action):
 		self.frame = 1
-		
+		holding.play("default")
 	elif Input.is_action_just_released(action):
 		self.frame = 0
-		 
+		holding.stop()
+		holding.frame = 0
--- scripts/Cursor.gd
+++ scripts/Cursor.gd
@@ -3,10 +3,42 @@ extends Node2D
 const ButtonHint = preload("res://objects/ButtonHint.tscn")
 
 export (float) var speed = 100
+export (NodePath) var pickerTip
+export (NodePath) var handTip
+export (NodePath) var drillTip
 
 var dir = Vector2.ZERO
 
 var button = ButtonHint.instance()
+var target
+var state
+
+func _ready():
+	button.get_node("Holding").connect("animation_finished", self, "_on_hold_succeeded")
+	_change_state("PICK")
+	
+func _change_state(state):
+	match self.state:
+		"PICK":
+			get_node("Picker").visible = false
+		"DRILL":
+			get_node("Drill").visible = false
+		"HAND":
+			get_node("Hand").visible = false
+	
+	self.state = state
+	
+	match self.state:
+		"PICK":
+			get_node("Picker").visible = true
+			get_node("Tip").transform = get_node(pickerTip).get_relative_transform_to_parent(self)
+		"DRILL":
+			get_node("Drill").visible = true
+			get_node("Tip").transform = get_node(drillTip).get_relative_transform_to_parent(self)
+		"HAND":
+			get_node("Hand").visible = true
+			get_node("Tip").transform = get_node(handTip).get_relative_transform_to_parent(self)
+			
 
 func _process(_delta):
 	update_direction()
@@ -31,7 +63,17 @@ func _on_Area2D_area_entered(area):
 	if not tooth.is_in_group("teeth"):
 		return
 		
-	tooth.add_child(button)
+	target = tooth
+		
+	target.add_child(button)
+	
+func _on_hold_succeeded():
+	target.remove_child(button)
+	button.get_node("Holding").frame = 0
+	
+	match self.state:
+		"PICK":
+			self._change_state("HAND")
 
 func _on_Tip_area_exited(area):
 	var tooth = area.get_parent()
