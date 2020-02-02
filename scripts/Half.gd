ee2aadc56d170255b88d8b5490620fb0bd66016b
LiquidLemon <liquidlemon@protonmail.com>
2020-02-02 13:53:05 +0100

    Change teeth generation algorithm

--- scripts/Half.gd
+++ scripts/Half.gd
@@ -9,21 +9,24 @@ var rng = RandomNumberGenerator.new()
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	rng.randomize()
-	var int2anim = {
-		0: "white",
-		1: "white",
-		2: "white",
-		3: "yellow",
-		4: "yellow",
-		5: "crack"	
-	}
+ 
 	var children = self.get_children()
+	var teeth = []
+	
 	for ch in children:
 		if ch.is_in_group("teeth"):
-			var rand = rng.randi_range(0, 5)
-			var anim = int2anim[rand]
-			ch.set_state(anim)
-	pass # Replace with function body.
+			teeth.push_back(ch)
+			ch.set_state("white")
+	
+	for _i in range(2):
+		var i = rng.randi_range(0, teeth.size() - 1)
+		if rng.randi_range(0, 1) == 0:
+			teeth[i].set_state("yellow")
+		else:
+			teeth[i].set_state("crack")
+		
+		teeth.remove(i)
+				
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
